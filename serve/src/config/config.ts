7f65a768bd37e16e3253a685ae0b608c4fff2105
zhr <email@qq.com>
2020-12-18 17:03:30 +0800

    创建数据库文件

---
+++ serve/src/config/config.ts
@@ -0,0 +1,19 @@
+import { config } from 'dotenv';
+config();
+
+export default {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
